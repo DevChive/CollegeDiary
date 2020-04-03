0b2dcfbba0ed91611e23a0eca6654db89b22d14b
Theodora Tataru <dora.theodora89@yahoo.com>
2020-04-03 20:28:48 +0100

    Firebase connection - Success, Adding a subject to the database - Success

---
+++ CD/CD/Helper/FireBaseHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Firebase.Database;
+using Firebase.Database.Query;
+using CD.Models;
+
+namespace CD.Helper
+
+{
+    class FireBaseHelper
+    {
+        private readonly string SubjectName = "Subject";
+        readonly FirebaseClient firebase = new FirebaseClient("https://collegediary-fd88a.firebaseio.com/Subjects");
+
+        public async Task<List<Subject>> GetAllSubjects()
+        {
+            return (await firebase.Child(SubjectName).OnceAsync<Subject>()).Select(item => new Subject
+            {
+                SubjectName = item.Object.SubjectName,
+                SubjectID = item.Object.SubjectID,
+                LecturerName = item.Object.LecturerName,
+                LecturerEmail = item.Object.LecturerEmail,
+                CA = item.Object.CA,
+                FinalExam = item.Object.FinalExam
+            }).ToList();
+        }
+
+        public async Task AddSubject(string subjectName, string lecturerName, string lecturerEmail, int ca, int finalExam)
+        {
+            await firebase.Child(subjectName).PostAsync(new Subject()
+            {
+                SubjectID = Guid.NewGuid(),
+                SubjectName = subjectName,
+                LecturerName = lecturerName,
+                LecturerEmail = lecturerEmail,
+                CA = ca,
+                FinalExam = finalExam
+            }); 
+        }
+
+        public async Task<Subject> GetSubject(Guid subjectID)
+        {
+            var allSubjects = await GetAllSubjects();
+            await firebase.Child(SubjectName).OnceAsync<Subject>();
+            return allSubjects.FirstOrDefault(a => a.SubjectID == subjectID);
+        }
+
+        public async Task<Subject> GetSubject(string subjectname)
+        {
+            var allSubjects = await GetAllSubjects();
+            await firebase.Child(SubjectName).OnceAsync<Subject>();
+            return allSubjects.FirstOrDefault(a => a.SubjectName == subjectname);
+        }
+    }
+}
--- CD/CD/Pages/AddSubject.xaml.cs
+++ CD/CD/Pages/AddSubject.xaml.cs
@@ -3,23 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Firebase.Database;
+using Firebase.Database.Query;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using CD.Models;
+using CD.Helper;
 
 namespace CD.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddSubject : ContentPage
     {
+
+        readonly FireBaseHelper fireBaseHelper = new FireBaseHelper();
+
         public AddSubject()
         {
             InitializeComponent();
         }
 
-        private void Save_Subject(object sender, EventArgs e)
+        private async void Save_Subject(object sender, EventArgs e)
         {
-            DisplayAlert("Subject Added", $"{this.subjectName.Text}\n{this.lecturerName.Text}","OK");
+            int CA = Int32.Parse(this.CA.Text);
+            int FinalExam = Int32.Parse(this.finalExam.Text);
+
+            if (CA + FinalExam == 100)
+            {
+                var subject = await fireBaseHelper.GetSubject(subjectName.Text);
+
+                await fireBaseHelper.AddSubject(subjectName.Text, lecturerName.Text, lecturerEmail.Text, CA, FinalExam);
+                await DisplayAlert("Subject Added", $"{this.subjectName.Text}\n{this.lecturerName.Text}", "OK");
+            }
+            else
+                await DisplayAlert("Subject not added!", $"Continuous Assessment = {this.CA.Text}%\n" +
+                    $"Final Exam = {this.finalExam.Text}%\nThey should add up to 100", "OK");
         }
     }
 }
