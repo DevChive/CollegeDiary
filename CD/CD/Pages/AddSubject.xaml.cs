20970565070e8a99a95298a90ead5e31c46ee17e
Suzukibud <damiendoran93@gmail.com>
2020-04-14 11:37:53 +0100

    6 - CA, Regex completed for added subject

--- CD/CD/Pages/AddSubject.xaml.cs
+++ CD/CD/Pages/AddSubject.xaml.cs
@@ -2,6 +2,7 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using CD.Helper;
+using System.Text.RegularExpressions;
 
 namespace CD.Pages
 {
@@ -15,16 +16,35 @@ namespace CD.Pages
         {
             InitializeComponent();// Loading this page
         }
+        
         private async void Save_Subject(object sender, EventArgs e)
         {
             bool validate = true;
+            string pattern = null;
+            pattern = "^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$";
+            var allSubjects = await fireBaseHelper.GetAllSubjects();
+            var listView = new ListView().ItemsSource = allSubjects;
 
 
-            if (string.IsNullOrEmpty(this.subjectName.Text) || string.IsNullOrEmpty(this.lecturerEmail.Text) ||
-                string.IsNullOrEmpty(this.lecturerName.Text) || string.IsNullOrEmpty(this.CA.Text) ||
-                string.IsNullOrEmpty(this.finalExam.Text))
+
+            if (string.IsNullOrEmpty(this.subjectName.Text) || string.IsNullOrEmpty(this.lecturerName.Text) || string.IsNullOrEmpty(this.CA.Text) ||
+                string.IsNullOrEmpty(this.finalExam.Text) || string.IsNullOrEmpty(this.lecturerEmail.Text))
             {
+                await DisplayAlert("Subject not added!", "All fields are required and the weight of the module needs to add up to 100", "OK");
                 validate = false;
+            }
+            else
+            {
+
+                if (Regex.IsMatch(this.lecturerEmail.Text, pattern) && !string.IsNullOrEmpty(this.lecturerEmail.Text) && validate)
+                {
+                    validate = true;
+                }
+                else
+                {
+                    await DisplayAlert(" ", "Invalid email address entered", "OK");
+                    validate = false;
+                }
             }
 
             if (validate)
@@ -32,19 +52,14 @@ namespace CD.Pages
                 int CA = Int32.Parse(this.CA.Text);
                 int FinalExam = Int32.Parse(this.finalExam.Text);
                 if (CA + FinalExam == 100)
-                {   
+                {
                     var subject = await fireBaseHelper.GetSubject(subjectName.Text);
 
                     await fireBaseHelper.AddSubject(subjectName.Text, lecturerName.Text, lecturerEmail.Text, CA, FinalExam);
                     await DisplayAlert("Subject Added", $"{this.subjectName.Text}\n{this.lecturerName.Text}", "OK");
                     await Navigation.PushAsync(new MainPage());
                 }
-                else
-                    await DisplayAlert("Subject not added!", "Modules weight need to add up to 100", "OK");
-
-            }
-            else
-                await DisplayAlert("Subject not added!","All fields are required", "OK");
+            }      
         }
 
         private async void Cancel_Subject(object sender, EventArgs e)
