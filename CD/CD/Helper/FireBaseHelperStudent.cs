23ae4f5f8729240d29a1c584cce2fc7f70331cb7
Theodora Tataru <dora.theodora89@yahoo.com>
2020-04-22 14:11:12 +0100

    CD-28. (MADE BY DAMIEN) - Student functionality

---
+++ CD/CD/Helper/FireBaseHelperStudent.cs
@@ -0,0 +1,62 @@
+﻿using CD.Models;
+using Firebase.Database;
+using Firebase.Database.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD.Helper
+{
+	class FireBaseHelperStudent
+	{
+		private readonly string Student_Name = "Students";
+		readonly FirebaseClient firebase = new FirebaseClient("https://collegediary-fd88a.firebaseio.com/");
+
+		public async Task<List<Student>> GetAllStudents()
+		{
+			return (await firebase.Child(Student_Name).OnceAsync<Student>()).Select(item => new Student
+			{
+				StudentName = item.Object.StudentName,
+				StudentID = item.Object.StudentID,
+				StudentEmail = item.Object.StudentEmail,
+				StudentAddress = item.Object.StudentAddress,
+				StudentPhone = item.Object.StudentPhone
+			}).ToList();
+		}
+
+		public async Task AddStudent(string studentName, string studentEmail, string studentAddress, int studentPhone)
+		{
+			await firebase.Child(Student_Name).PostAsync(new Student()
+			{
+				StudentID = Guid.NewGuid(),
+				StudentName = studentName,
+				StudentEmail = studentEmail,
+				StudentAddress = studentAddress,
+				StudentPhone = studentPhone
+			}) ;
+		}
+
+		public async Task<Student> GetStudent(Guid studentID)
+		{
+			var allStudents = await GetAllStudents();
+			await firebase.Child(Student_Name).OnceAsync<Student>();
+			return allStudents.FirstOrDefault(a => a.StudentID == studentID);
+		}
+
+		public async Task<Student> GetStudent(string studentName)
+		{
+			var allStudents = await GetAllStudents();
+			await firebase.Child(Student_Name).OnceAsync<Student>();
+			return allStudents.FirstOrDefault(a => a.StudentName == studentName);
+		}
+
+		public async Task DeleteStudent(Guid StudentID)
+		{
+			var toDeleteStudent = (await firebase.Child(Student_Name).OnceAsync<Subject>()).FirstOrDefault
+				(a => a.Object.SubjectID == StudentID);
+			await firebase.Child(Student_Name).Child(toDeleteStudent.Key).DeleteAsync();
+		}
+	}
+}
---
+++ CD/CD/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CD.Models
+{
+	class Student
+	{
+		public Guid StudentID { get; set; }
+		public String StudentName { get; set; }
+		public String StudentAddress { get; set; }
+		public int StudentPhone { get; set; }
+		public string StudentEmail { get; set; }
+	}
+}
